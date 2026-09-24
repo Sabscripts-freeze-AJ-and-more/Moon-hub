6a3fc651e0c51761302e6aa1bf5a7b045111b06a
Takebylike <leonardoviragos1@gmail.com>
2026-09-23 22:55:39 -0600

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("http://moonware-obfuscator-fanz.onrender.com/v/80ece2af4ba491e0"))()
+loadstring(game:HttpGet("https://gist.githubusercontent.com/scripts-api-true/1a462e30b7775150fd61f7b08d259e66/raw/a7dc522485f866d4f6fcd27e13940aa689482abb/Scripter_s-hub.lua"))()
