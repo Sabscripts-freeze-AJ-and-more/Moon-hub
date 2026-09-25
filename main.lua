d36c5aa07c4125c11cf02187a6fe509f42d7569b
Takebylike <leonardoviragos1@gmail.com>
2026-09-24 19:41:02 -0600

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://gist.githubusercontent.com/scripts-api-true/75c155bbb0955aa6f63812c5c145f18d/raw/bd957d4dc95525395f8b446daac32754991aeaa7/Scripter_s-hub.lua"))()
+loadstring(game:HttpGet("https://gist.githubusercontent.com/scripts-api-true/ff4197dd54c95ffefbc675f6acb3c6f5/raw/e6a132e7d104f10b43098bf6b8cce7387bd7eecf/Scripter_s-hub.lua"))()
